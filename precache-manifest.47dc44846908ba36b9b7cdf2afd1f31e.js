4664f872b0de9a88ef0f46065c4f600b36f92758
Sylvia Garcia-Chavez <sgarciachavez@iMac.attlocal.net>
2020-06-09 17:27:47 -0500

    Updates

---
+++ precache-manifest.47dc44846908ba36b9b7cdf2afd1f31e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e954cca0b093f3420dfa623864b6b0ab",
+    "url": "/WhereInTheWorld/index.html"
+  },
+  {
+    "revision": "06d3b21cf3058968b02d",
+    "url": "/WhereInTheWorld/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "58100d7ae72b270699d4",
+    "url": "/WhereInTheWorld/static/css/main.b5b4601a.chunk.css"
+  },
+  {
+    "revision": "06d3b21cf3058968b02d",
+    "url": "/WhereInTheWorld/static/js/2.5b157a5e.chunk.js"
+  },
+  {
+    "revision": "58100d7ae72b270699d4",
+    "url": "/WhereInTheWorld/static/js/main.adc04f72.chunk.js"
+  },
+  {
+    "revision": "f52d577a9b4db7d8c543",
+    "url": "/WhereInTheWorld/static/js/runtime-main.fef0df9e.js"
+  },
+  {
+    "revision": "fc68b46a7dac8577dc3825f84d452b14",
+    "url": "/WhereInTheWorld/static/media/left-arrow-blk.fc68b46a.svg"
+  },
+  {
+    "revision": "c5dfb36064ccc8cabb45ea77de3ac339",
+    "url": "/WhereInTheWorld/static/media/left-arrow.c5dfb360.svg"
+  },
+  {
+    "revision": "6650aba86e27ff49bc6ad1b57ca0ec0f",
+    "url": "/WhereInTheWorld/static/media/mag-glass.6650aba8.svg"
+  },
+  {
+    "revision": "6f5fc00b83394e678e5ef4786168275d",
+    "url": "/WhereInTheWorld/static/media/mag-glass_blk.6f5fc00b.svg"
+  },
+  {
+    "revision": "74556d3138aa6b2c347058cfa1c50521",
+    "url": "/WhereInTheWorld/static/media/moon-white.74556d31.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/WhereInTheWorld/precache-manifest.47dc44846908ba36b9b7cdf2afd1f31e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WhereInTheWorld/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
